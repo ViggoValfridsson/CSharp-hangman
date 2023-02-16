1ef01f2918336c8fc04a8f7bfa1c7930e4a27cde
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-02-16 10:31:54 +0100

    added api-key to secrets

---
+++ hangman/Fetch/GetRandomWord.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+namespace hangman.Fetch;
+
+public class GetRandomWord
+{
+    public static void FetchWord()
+    {
+        //var builder = WebApplication.CreateBuilder();
+        //var movieApiKey = builder.Configuration["APINinjas:ApiKey"];
+
+        //var app = builder.Build();
+
+        //app.MapGet("/", () => movieApiKey);
+
+        //app.Run();
+
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+        string apiKey = config["APINinjas:ApiKey"];
+        Console.WriteLine(apiKey + "api");
+
+    }
+}
