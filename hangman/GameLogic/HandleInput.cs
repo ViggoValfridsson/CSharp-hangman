8a3726b9539ee7b30d883405c7b3d05801da4f24
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-02-16 14:13:24 +0100

    Created game logic

---
+++ hangman/GameLogic/HandleInput.cs
@@ -0,0 +1,47 @@
+﻿namespace hangman.GameLogic;
+
+public class HandleInput
+{
+    public static char GetInput(List<char> correctLetters, List<char> incorrectLetters)
+    {
+        while (true)
+        {
+            WriteLine("Enter your guess: ");
+            string result = ReadLine()!;
+            
+            try
+            {
+                char resultLetter = Convert.ToChar(result);
+                
+                if(!char.IsLetter(resultLetter))
+                {
+                    throw new ArgumentException("Enter a valid letter!");
+                }
+
+                if (correctLetters.Contains(resultLetter) || incorrectLetters.Contains(resultLetter))
+                {
+                    throw new ArgumentException("You have already entered this letter!");
+                }
+
+                return Char.ToLower(resultLetter); 
+            }
+            catch (ArgumentException e)
+            {
+                WriteLine(e.Message);
+            }
+            catch 
+            { 
+                WriteLine("Enter a valid letter!");
+            }
+        }
+    }
+    public static bool IsCorrectGuess(char input, string secretword)
+    {
+        if (secretword.Contains(input))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
